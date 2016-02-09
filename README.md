0f64357015e5424d1f247c7bc796499dcad68d6f
Dylan Richardson <dylanrichardson47@gmail.com>
2016-02-09 18:32:35 -0500

    Add maven support and enhance readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# p2p-app
+This is a basic person-to-person file sharing application built in Java for our Computer Networks class.
---
+++ src/main/java/com/cnt5106/p2p/App.java
@@ -0,0 +1,13 @@
+package com.cnt5106.p2p;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
