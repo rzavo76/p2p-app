33160cc4eebe85e66578c50f979aa66357865302
Ryan Zavoral <rzavo76@ufl.edu>
2016-03-12 14:45:39 -0500

    Added toString and get/set functions for the threads

--- src/main/java/com/cnt5106/p2p/Sender.java
+++ src/main/java/com/cnt5106/p2p/Sender.java
@@ -8,8 +8,28 @@ import java.io.*;
 import java.lang.*;
 
 public class Sender extends NetworkThread {
-	Sender(int port, String hostname) {
+	private int targetPort;
+	private String targetHostname;
+	Sender(int port, String hostname, int targetPort, String targetHostname) {
 		super(port, hostname);
-}
+		this.targetPort = targetPort;
+		this.targetHostname = targetHostname;
+	}
+	public int getTargetPort() {
+		return targetPort;
+	}
+	public String getTargetHostname() {
+		return targetHostname;
+	}
+	public void setTargetPort(int targetPort) {
+		this.targetPort = targetPort;
+	}
+	public void setTargetHostname(String targetHostname) {
+		this.targetHostname = targetHostname;
+	}
+	public String toString() {
+		return "The port is " + port + " and the hostname is " + hostname + "." + 
+		"The target port is " + targetPort + " and the target hostname is " + targetHostname + "."; 
+	}
 	public void run() {}
 }
