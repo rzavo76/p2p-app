fb6c81c2f7e9af7c9ea4cc5a8f11706886ae667f
Ryan Zavoral <rzavo76@ufl.edu>
2016-03-12 14:34:59 -0500

    Added sender and receiver threads

---
+++ src/main/java/com/cnt5106/p2p/Sender.java
@@ -0,0 +1,15 @@
+package com.cnt5106.p2p;
+
+// Sender class created by Ryan Zavoral Feb. 16, 2016.
+
+import java.text.*;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Sender extends NetworkThread {
+	Sender(int port, String hostname) {
+		super(port, hostname);
+}
+	public void run() {}
+}
