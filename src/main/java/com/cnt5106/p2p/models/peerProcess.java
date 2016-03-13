c46fbd5f8226d9a7f99b980b3f44674830378002
Ryan Zavoral <rzavo76@ufl.edu>
2016-03-13 03:21:03 -0400

    Updated peerProcess with bit field and common config data

--- src/main/java/com/cnt5106/p2p/models/peerProcess.java
+++ src/main/java/com/cnt5106/p2p/models/peerProcess.java
@@ -6,24 +6,29 @@ package com.cnt5106.p2p.models;
  * A model class for containing information about peers
  */
 
+
+import java.util.*;
+import java.lang.*;
+
 public class peerProcess {
 
-    //PeerInfo data
+    // PeerInfo data
     private int pID;
     private String hostName;
     private int lPort;
     private Boolean hasFile;
     
-    //Common data
+    // Common data
     private int preferredNeighbors;
     private int unchokingInterval;
-    private int optimisticUnchokingInterval;
+    private int optUnchokingInterval;
     private String fileName;
     private int fileSize;
     private int pieceSize;
 
-    //bit field
-    //thread manager
+    // File piece bit field
+    boolean[] pieces;
+    // Thread manager
 
     public static void main(String[] args) {
         // Read PeerInfo.cfg to match arg[0] to pID and store hostname, lport, hasfile
@@ -31,12 +36,27 @@ public class peerProcess {
         // Connect to previously made connections
     }
 
-    public peerProcess(int pID, String hostName, int lPort, Boolean hasFile)
+    public peerProcess(int pID, String hostName, int lPort, 
+        Boolean hasFile, int preferredNeighbors, 
+        int unchokingInterval, int optUnchokingInterval, 
+        String fileName, int fileSize, int pieceSize)
     {
         this.pID = pID;
         this.hostName = hostName;
         this.lPort = lPort;
         this.hasFile = hasFile;
+
+        this.preferredNeighbors = preferredNeighbors;
+        this.unchokingInterval = unchokingInterval;
+        this.optUnchokingInterval = optUnchokingInterval;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.pieceSize = pieceSize;
+        
+        pieces = new boolean[Math.ceil((double)fileSize/pieceSize)];
+        if(hasFile) {
+            Arrays.fill(pieces, hasFile);
+        }
     }
 
     public void setPID(int pID)
